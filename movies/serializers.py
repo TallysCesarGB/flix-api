6681941e597af22c891abcd9602796d0399a33a4
Tallys Cesar <tallyscesardev@gmail.com>
2026-03-03 13:51:01 -0300

    Enhance MovieSerializer with rating calculation and release date validation

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
+import datetime
+from django.db.models import Avg
 from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        ratings = obj.reviews.all()
+        if ratings.exists():
+            return round(ratings.aggregate(average=Avg('rating'))['average'], 1)
+        return None
+
+    def validate_release_date(self, value):
+        if value and value < '1888-01-01' or value > datetime.date.today():
+            return serializers.ValidationError("Release date must be between January 1, 1888 and today.")
+        return value

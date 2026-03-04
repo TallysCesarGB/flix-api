5c070c044ae08d96ad172d21cf85c79546044f88
Tallys Cesar <tallyscesardev@gmail.com>
2026-03-04 09:50:53 -0300

    Fix rating calculation and release date validation in MovieSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -13,10 +13,10 @@ class MovieSerializer(serializers.ModelSerializer):
     def get_rate(self, obj):
         ratings = obj.reviews.all()
         if ratings.exists():
-            return round(ratings.aggregate(average=Avg('rating'))['rating__avg'], 1)
+            return round(ratings.aggregate(average=Avg('rating'))['average'], 1)
         return None
 
     def validate_release_date(self, value):
-        if value and value < '1888-01-01' or value > datetime.date.today():
+        if value and (value < datetime.data(1888, 1, 1) or value > datetime.date.today()):
             return serializers.ValidationError("Release date must be between January 1, 1888 and today.")
         return value

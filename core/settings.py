2831d126d22d5f9ae3606ddb03a3ed4acc0e53f2
Tallys Cesar <tallyscesardev@gmail.com>
2026-03-17 14:19:05 -0300

    update token lifetime

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,6 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(weeks=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=2),
 }
